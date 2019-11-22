51c5001bfece08f1e197fabde622b7ca1f096523
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-11-22 18:59:30 -0500

    Passing Data between VC

--- swoosh/swoosh/Controller/LeagueVC.swift
+++ swoosh/swoosh/Controller/LeagueVC.swift
@@ -38,17 +38,13 @@ class LeagueVC: UIViewController {
     
     
     @IBAction func onNextTapped(_ sender: Any) {
-     performSegue(withIdentifier: "skillVCSegue", sender: self)
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
     }
-    */
 
 }
