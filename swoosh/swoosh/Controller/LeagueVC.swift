9f813b7d135b104c1b98946aa0e18dc67763f8de
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-11-22 18:38:55 -0500

    Struct for data

--- swoosh/swoosh/Controller/LeagueVC.swift
+++ swoosh/swoosh/Controller/LeagueVC.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        player = Player()
     }
+    @IBAction func onMenTapped(_ sender: Any) {
+        selectLeague(selectedLeague: "men")
+    }
+    
+    @IBAction func onWomenTapped(_ sender: Any) {
+        selectLeague(selectedLeague: "women")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(selectedLeague: "coed")
+    }
+    
+    func selectLeague(selectedLeague: String){
+        player.desiredLeague = selectedLeague
+        nextBtn.isEnabled = true
+    }
+    
     
     @IBAction func onNextTapped(_ sender: Any) {
      performSegue(withIdentifier: "skillVCSegue", sender: self)
